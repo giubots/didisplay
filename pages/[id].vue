cf2e736dfacf26ff375829138a6e217987c94cfd
giubots <giubots@protonmail.com>
2023-03-29 14:37:48 +0000

    Add routing and server rendering

--- pages/[id].vue
+++ pages/[id].vue
@@ -2,14 +2,7 @@
   <h1 style="padding: 50px">{{ message }}</h1>
 </template>
 
-<script>
-export default {
-  data() {
-    return { message: "hello" };
-  },
-  async mounted() {
-    this.message = await $fetch("/api/1");
-    setTimeout(() => location.reload(), 1000);
-  },
-};
+<script setup>
+const route = useRoute();
+const { data: message } = await useFetch(`/api/${route.params.id}`);
 </script>
